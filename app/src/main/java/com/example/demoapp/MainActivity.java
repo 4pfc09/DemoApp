1d5f028e1ec29ae13802698d5652514d7663c481
4pfc09 <4pfc09@gmail.com>
2022-09-07 13:32:55 +0200

    DemoApp: Commit inicial

---
+++ app/src/main/java/com/example/demoapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.demoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnToTabbedView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnToTabbedView = findViewById(R.id.btnToTabs);
+        btnToTabbedView.setOnClickListener(a -> goToTabbedActivity());
+    }
+
+    private void goToTabbedActivity() {
+        Intent intent = new Intent(this, SimpleTabbedActivity.class);
+        startActivity(intent);
+    }
+}
